b63ce7036d93ae36cb06d8ea9a558e705e35d35c
Srirag Hariharan <sriraghariharan108@gmail.com>
2025-03-20 17:11:03 +0530

    Updated the backend URL from localhost to the production environment.

--- src/constants/constants.js
+++ src/constants/constants.js
@@ -1,3 +1,8 @@
 export const DEFAULT_PROFILE_IMAGE = "https://leaf-user-profile-pics.s3.us-east-1.amazonaws.com/default-avatar.jpg";
-export const LEAF_USER_BASE_URL = "http://localhost:2000/api/v1/user"
-export const LEAF_PROFILE_REFRESH_TOKEN_URL = "http://localhost:2000/api/v1/user/auth/refresh-token"
+// // dev
+// export const LEAF_USER_BASE_URL = "http://localhost:2000/api/v1/user"
+// export const LEAF_PROFILE_REFRESH_TOKEN_URL = "http://localhost:2000/api/v1/user/auth/refresh-token"
+
+// // prod
+export const LEAF_USER_BASE_URL = "https://api.leaf.monster/api/v1/user"
+export const LEAF_PROFILE_REFRESH_TOKEN_URL = "https://api.leaf.monster/api/v1/user/auth/refresh-token"
